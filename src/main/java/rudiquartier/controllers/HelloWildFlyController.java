7226465931b9dc3ea21cc22670bf2af8e92085f1
Nicolas Quartier <quartier.nicolas@gmail.com>
2016-12-17 17:07:41 +0100

    Add requestmapping, pointing to index.html

--- src/main/java/rudiquartier/controllers/HelloWildFlyController.java
+++ src/main/java/rudiquartier/controllers/HelloWildFlyController.java
@@ -1,6 +1,7 @@
 package rudiquartier.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class HelloWildFlyController {
     @RequestMapping("hello")
     public String sayHello(){
         return ("Hello, It's me, SpringBoot on Wildfly (3: with webconfig)");
+    }
+
+    @RequestMapping(value = "index", method = RequestMethod.GET)
+    public String index(){
+        return "index";
     }
 }
