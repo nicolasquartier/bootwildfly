ab94740288a1d2410234d46ad0eb0b361e0d90bd
Nicolas Quartier <quartier.nicolas@gmail.com>
2016-12-17 17:33:52 +0100

    Updated gitignore

--- src/main/java/rudiquartier/controllers/HelloWildFlyController.java
+++ src/main/java/rudiquartier/controllers/HelloWildFlyController.java
@@ -1,10 +1,12 @@
 package rudiquartier.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
+@RequestMapping("/")
 public class HelloWildFlyController {
 
 
